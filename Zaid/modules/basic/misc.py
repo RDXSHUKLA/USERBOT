3eb49f64caa3f4ac25a3eb37ddb15c7d6efdf2c7
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-11-14 00:01:38 +0530

    Update misc.py

--- Zaid/modules/basic/misc.py
+++ Zaid/modules/basic/misc.py
@@ -11,7 +11,7 @@ from Zaid.helper.PyroHelpers import ReplyCheck
 from Zaid.modules.help import add_command_help
 from Zaid.modules.bot.inline import get_readable_time
 
-alive_logo = ALIVE_PIC or "https://telegra.ph/file/cc0890d0876bc18c19e05.jpg"
+alive_logo = ALIVE_PIC or "https://graph.org/file/c6a2ed96648fd03377dc9.jpg"
 
 if ALIVE_TEXT:
    txt = ALIVE_TEXT
