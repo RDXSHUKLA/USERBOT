a143dca053428923ed7b9d103fe5f0153fb4f198
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-11-14 11:49:16 +0530

    Update oneword.py

--- Zaid/modules/spam/oneword.py
+++ Zaid/modules/spam/oneword.py
@@ -1,72 +1,50 @@
-import random
-import time
-from pyrogram.types import Message
-from random import choice
-from pyrogram.errors import FloodWait
+import os
+import asyncio
 from pyrogram.types import Message
 from pyrogram import filters, Client
-
-# import 
-from Zaid import SUDO_USER 
-from cache.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
+from pyrogram.errors import FloodWait
+from ... import app, SUDO_USER
+from ... import *
 from cache.data import *
 
-@Client.on_message(
-    filters.command(["randi"], ".") & (filters.me | filters.user(SUDO_USER))
-)
-async def raid(xspam: Client, e: Message):  
-      Zaid = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
-      if len(Zaid) == 2:
-          counts = int(Zaid[0])
-          if int(e.chat.id) in GROUP:
-               return await e.reply_text("**Sorry !! i Can't Spam Here.**")
-          ok = await xspam.get_users(Zaid[1])
-          id = ok.id
-#          try:
-#              userz = await xspam.get_users(id)
-#          except:
-#              await e.reply(f"`404 : User Doesn't Exists In This Chat !`")
-#              return #remove # to enable this
-          if int(id) in VERIFIED_USERS:
-                text = f"Chal Chal baap Ko mat sikha😈"
-                await e.reply_text(text)
-          elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of ◉ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 ◉"
-                await e.reply_text(text)
-          else:
-              fname = ok.first_name
-              mention = f"[{fname}](tg://user?id={id})"
-              for _ in range(counts):
-                    reply = choice(OneWord)
-                    msg = f"{mention} {reply}"
-                    await xspam.send_message(e.chat.id, msg)
-                    await asyncio.sleep(0.10)
-      elif e.reply_to_message:
-          msg_id = e.reply_to_message.from_user.id
-          counts = int(Zaid[0])
-          if int(e.chat.id) in GROUP:
-               return await e.reply_text("**Sorry !! i Can't Spam Here.**")
-          user_id = e.reply_to_message.from_user.id
-          ok = await xspam.get_users(user_id)
-          id = ok.id
-          try:
-              userz = await xspam.get_users(id)
-          except:
-              await e.reply(f"`404 : User Doesn't Exists In This Chat !`")
-              return
-          if int(id) in VERIFIED_USERS:
-                text = f"Chal Chal baap Ko mat sikha😈"
-                await e.reply_text(text)
-          elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of ◉ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 ◉."
-                await e.reply_text(text)
-          else:
-              fname = ok.first_name
-              mention = f"[{fname}](tg://user?id={id})"
-              for _ in range(counts):
-                    reply = choice(OneWord)
-                    msg = f"{mention} {reply}"
-                    await xspam.send_message(e.chat.id, msg)
-                    await asyncio.sleep(0.10)
-      else:
-          await e.reply_text("Usage: .randi count username")
+
+FC = 2
+
+
+@app.on_message(cdz(["randi"])  & (filters.me | filters.user(SUDO_USER)))
+async def alt_lol(xspam: Client, message: Message):    
+    chat_id = message.chat.id
+    RUSH = None
+    if message.reply_to_message:
+        RUSH = message.reply_to_message.id
+    try:
+        for word in OneWord:
+            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
+            await asyncio.sleep(1)
+    except FloodWait:
+        print("Flood !!")
+        pass
+
+
+
+@app.on_message(cdz(["randii"])  & (filters.me | filters.user(SUDO_USER)))
+async def alt_mkc(xspam: Client, message: Message):    
+    chat_id = message.chat.id
+    RUSH = None
+    if message.reply_to_message:
+        RUSH = message.reply_to_message.id
+    try:
+        for word in OneWord:
+            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
+            await asyncio.sleep(000.1)
+    except FloodWait:
+        print("Flood !!")
+        pass
+
+
+
+@app.on_message(cdz(["stop"])  & (filters.me | filters.user(SUDO_USER)))
+async def alt_stop(_, message: Message):    
+    reply = await message.reply_text("sᴛᴏᴘᴘɪɴɢ ...")
+    await reply.edit("sᴛᴏᴘᴘᴇᴅ !!\n\nᴡᴀɪᴛ ғᴏʀ 2 ᴍɪɴᴜᴛᴇs !!")
+    os.system(f"kill -9 {os.getpid()} && python3 -m SHUKLA")
