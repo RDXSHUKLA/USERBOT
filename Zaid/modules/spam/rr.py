229d024c2cadaa810825cfdc026ea7bd17242075
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-11-14 11:32:39 +0530

    Update rr.py

--- Zaid/modules/spam/rr.py
+++ Zaid/modules/spam/rr.py
@@ -9,7 +9,7 @@ from cache.data import *
 ACTIVATE_RLIST = []
 
 
-@Client.on_message(filters.command("rr", prefixes=".") & (filters.me | filters.user(SUDO_USER))
+@Client.on_message(filters.command("rraid", prefixes=".") & (filters.me | filters.user(SUDO_USER))
 )
 async def rr(client: Client, message: Message):
     r = await message.edit_text("**Processing**")
@@ -37,7 +37,7 @@ async def rr(client: Client, message: Message):
     ACTIVATE_RLIST.append(user.id)
     await r.edit(f"**Replyraid Activated On {user.first_name} Successfully ✅**")
 
-@Client.on_message(filters.command("drr", prefixes=".") & (filters.me | filters.user(SUDO_USER))
+@Client.on_message(filters.command("drraid", prefixes=".") & (filters.me | filters.user(SUDO_USER))
 )
 async def drr(client: Client, message: Message):
     r = await message.edit_text("**Processing**")
