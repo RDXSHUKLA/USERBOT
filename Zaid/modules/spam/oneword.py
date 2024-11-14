43b2f2deb9ee78d91202e44f7f57cb344ddf9f9f
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-11-14 12:23:00 +0530

    Update oneword.py

--- Zaid/modules/spam/oneword.py
+++ Zaid/modules/spam/oneword.py
@@ -1,50 +1,88 @@
-import os
-import asyncio
-from pyrogram.types import Message
+from random import choice
 from pyrogram import filters, Client
-from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+# import 
 from Zaid import SUDO_USER 
 from cache.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
 from cache.data import *
 
+ACTIVATE_RLIST = []
 
-FC = 2
+
+@Client.on_message(filters.command("hiraid", prefixes=".") & (filters.me | filters.user(SUDO_USER))
+)
+async def rr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user.id
+    else:
+        user = message.text.split(None, 1)[1]
+        if not user:
+            await r.edit("**Provide Me A USER_ID or reply to someone**")
+            return
+    user = await client.get_users(user)
+    if int(message.chat.id) in GROUP:
+        await r.edit("`You Cannot Spam In Developers' Chat`")
+        return
+    if int(user.id) in VERIFIED_USERS:
+        await r.edit("You Cannot Spam On Developers")
+        return
+    elif int(user.id) in SUDO_USER:
+        await r.edit("That Guy Is part of sudo user.")
+        return
+    elif int(user.id) in ACTIVATE_RLIST:
+        await r.edit("User Already in Raidlist.")
+        return
+    ACTIVATE_RLIST.append(user.id)
+    await r.edit(f"**Hindiraid Activated On {user.first_name} Successfully ✅**")
+
+@Client.on_message(filters.command("dhiraid", prefixes=".") & (filters.me | filters.user(SUDO_USER))
+)
+async def drr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user.id
+    else:
+        user = message.text.split(None, 1)[1]
+        if not user:
+            await r.edit("Provide me username/userid or reply to user for deactivating Hindiraid")
+            return
+    user = await client.get_users(user)
+    if int(user.id) not in ACTIVATE_RLIST:
+        await r.edit("User Not in Replyraid.")
+        return
+    ACTIVATE_RLIST.remove(user.id)
+    await r.edit(f"**Hindi Raid has Been Removed {user.first_name}, enjoy!**")
 
 
-@Client.on_message(filters.command("randi", prefixes=".") & (filters.me | filters.user(SUDO_USER))
-async def alt_lol(xspam: Client, message: Message):    
-    chat_id = message.chat.id
-    RUSH = None
-    if message.reply_to_message:
-        RUSH = message.reply_to_message.id
+@Client.on_message(filters.incoming)
+async def watch_raids(client: Client, message: Message):
     try:
-        for word in OneWord:
-            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
-            await asyncio.sleep(1)
-    except FloodWait:
-        print("Flood !!")
-        pass
-
-
-
-@Client.on_message(filters.command("land", prefixes=".") & (filters.me | filters.user(SUDO_USER))
-async def alt_mkc(xspam: Client, message: Message):    
-    chat_id = message.chat.id
-    RUSH = None
-    if message.reply_to_message:
-        RUSH = message.reply_to_message.id
-    try:
-        for word in OneWord:
-            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
-            await asyncio.sleep(000.1)
-    except FloodWait:
-        print("Flood !!")
-        pass
-
-
-
-@Client.on_message(filters.command("stopp", prefixes=".") & (filters.me | filters.user(SUDO_USER))
-async def alt_stop(_, message: Message):    
-    reply = await message.reply_text("sᴛᴏᴘᴘɪɴɢ ...")
-    await reply.edit("sᴛᴏᴘᴘᴇᴅ !!\n\nᴡᴀɪᴛ ғᴏʀ 2 ᴍɪɴᴜᴛᴇs !!")
-    os.system(f"kill -9 {os.getpid()} && python3 -m Zaid")
+        if not message:
+            return
+        if not message.from_user:
+            return
+        user = message.from_user.id
+        userr = message.from_user
+        mention = f"[{userr.first_name}](tg://user?id={userr.id})"
+        raid = f"{mention} {choice(HIRAID)}"
+        if int(user) in VERIFIED_USERS:
+            return
+        elif int(user) in SUDO_USER:
+            return
+        if int(message.chat.id) in GROUP:
+            return
+        try:
+            if not message.from_user.id in ACTIVATE_RLIST:
+                return
+        except AttributeError:
+            return
+        try:
+            if message.from_user.id in ACTIVATE_RLIST:
+                await message.reply_text(raid)
+        except Exception as a:
+            print(f"An error occurred (a): {str(a)}")
+    except Exception as b:
+        print(f"An error occurred (b): {str(b)}")
